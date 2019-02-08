8413953b592eb8ab5d70d26f856fdd71e247d081
Antonin RAFFIN <antonin.raffin@ensta.org>
2019-02-08 17:04:39 +0100

    Update version name

--- setup.py
+++ setup.py
@@ -136,7 +136,7 @@ setup(name='stable_baselines',
       license="MIT",
       long_description=long_description,
       long_description_content_type='text/markdown',
-      version="2.4.1a",
+      version="2.4.1a0",
       )
 
 # python setup.py sdist
